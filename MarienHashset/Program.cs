1074fddc32b995969679a1432bd5a9c8bf9770f3
Ortiz Marvin <105992216+MarvinOrtiz18@users.noreply.github.com>
2025-10-15 22:27:25 -0600

    Initial project setup

---
+++ MarienHashset/Program.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="UNAN Managua CUR-Carazo">
+//     Author: MAOG18
+//     Copyright (c) . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarienHashset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
